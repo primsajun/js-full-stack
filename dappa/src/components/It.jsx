2ef333fd5555009573b9ea16d3b0add71e901e4f
prim sajun <138086244+primsajun@users.noreply.github.com>
2025-12-16 10:13:58 +0000

    second step

---
+++ dappa/src/components/It.jsx
@@ -0,0 +1,10 @@
+function It(){
+    return (
+        <div>
+            <h1>IT: welcome to derry</h1>
+            <p>IT: Welcome to Derry is a supernatural horror television series that acts as a prequel to the IT films (IT 2017 and IT Chapter Two 2019). It premiered on HBO / HBO Max in October 2025 and explores the terrifying earlier history of Derry, Maine — the cursed town at the center of Stephen King’s horror mythos</p>
+        </div>
+    )
+}
+
+export default It
